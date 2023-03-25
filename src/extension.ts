4f49c5e15278319982e2d9b203d8f21ea921506d
Wenbin Liu <liuwenbin.lwb@gmail.com>
2023-03-25 23:36:51 +0800

    fix "io" error and format code

--- src/extension.ts
+++ src/extension.ts
@@ -1,109 +1,119 @@
-import * as vscode from 'vscode';
-import * as openai from 'openai';
-import * as io from 'socket.io-client';
+import * as vscode from "vscode";
+import * as openai from "openai";
+import * as io from "socket.io-client";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "MyChatGPT" is now active!');
+  console.log('Congratulations, your extension "MyChatGPT" is now active!');
 
-    let disposable = vscode.commands.registerCommand('extension.myChatGPT', () => {
-        const openAIKey = vscode.window.showInputBox({
-            prompt: 'Please enter your OpenAI API key',
+  let disposable = vscode.commands.registerCommand(
+    "extension.myChatGPT",
+    () => {
+      const openAIKey = vscode.window.showInputBox({
+        prompt: "Please enter your OpenAI API key",
+      });
+
+      const chatPrompt = vscode.window.showInputBox({
+        prompt: "Please enter your chat prompt",
+      });
+
+      Promise.all([openAIKey, chatPrompt]).then((values) => {
+        const [key, prompt] = values;
+
+        if (!key || !prompt) {
+          vscode.window.showErrorMessage(
+            "Please provide an API key and a chat prompt"
+          );
+          return;
+        }
+
+        const socket = io.default("https://socket.openai.com", {
+          path: "/socket.io",
+          transports: ["websocket"],
         });
 
-        const chatPrompt = vscode.window.showInputBox({
-            prompt: 'Please enter your chat prompt',
+        const model = "text-davinci-002";
+        const parameters = {
+          engine: "text-davinci-002",
+          prompt,
+          max_tokens: 150,
+          temperature: 0.5,
+          n: 1,
+          stop: "\n",
+        };
+
+        openai.apiKey = key;
+
+        socket.on("connect", () => {
+          socket.emit("client-first-connect", {
+            client: "vscode-plugin",
+          });
+
+          socket.emit("get-models", {
+            clientId: socket.id,
+          });
+
+          socket.emit("start-generation", {
+            ...parameters,
+            model,
+            clientId: socket.id,
+          });
         });
 
-        Promise.all([openAIKey, chatPrompt]).then((values) => {
-            const [key, prompt] = values;
+        socket.on("disconnect", () => {
+          vscode.window.showInformationMessage("Disconnected from OpenAI API");
+        });
 
-            if (!key || !prompt) {
-                vscode.window.showErrorMessage('Please provide an API key and a chat prompt');
-                return;
+        socket.on(
+          "model-list",
+          (models: { id: string; objectName: string }[]) => {
+            if (!models || !models.length) {
+              vscode.window.showErrorMessage(
+                "Could not retrieve available models"
+              );
+              return;
             }
 
-            const socket = io('https://socket.openai.com', {
-                path: '/socket.io',
-                transports: ['websocket'],
-            });
+            const items = models.map((model) => ({
+              label: model.objectName,
+              detail: model.id,
+            }));
 
-            const model = 'text-davinci-002';
-            const parameters = {
-                engine: 'text-davinci-002',
-                prompt,
-                max_tokens: 150,
-                temperature: 0.5,
-                n: 1,
-                stop: '\n',
-            };
-
-            openai.apiKey = key;
-
-            socket.on('connect', () => {
-                socket.emit('client-first-connect', {
-                    client: 'vscode-plugin',
-                });
-
-                socket.emit('get-models', {
-                    clientId: socket.id,
-                });
-
-                socket.emit('start-generation', {
-                    ...parameters,
-                    model,
-                    clientId: socket.id,
-                });
-            });
-
-            socket.on('disconnect', () => {
-                vscode.window.showInformationMessage('Disconnected from OpenAI API');
-            });
-
-            socket.on('model-list', (models: { id: string; objectName: string }[]) => {
-                if (!models || !models.length) {
-                    vscode.window.showErrorMessage('Could not retrieve available models');
-                    return;
+            vscode.window
+              .showQuickPick(items, {
+                placeHolder: "Please select a GPT-3 model",
+              })
+              .then((selected) => {
+                if (!selected) {
+                  vscode.window.showErrorMessage("Please select a GPT-3 model");
+                  return;
                 }
 
-                const items = models.map((model) => ({
-                    label: model.objectName,
-                    detail: model.id,
-                }));
+                parameters.engine = selected.detail;
+                parameters.model = selected.detail;
 
-                vscode.window
-                    .showQuickPick(items, {
-                        placeHolder: 'Please select a GPT-3 model',
-                    })
-                    .then((selected) => {
-                        if (!selected) {
-                            vscode.window.showErrorMessage('Please select a GPT-3 model');
-                            return;
-                        }
+                socket.emit("start-generation", {
+                  ...parameters,
+                  model: selected.detail,
+                  clientId: socket.id,
+                });
+              });
+          }
+        );
 
-                        parameters.engine = selected.detail;
-                        parameters.model = selected.detail;
-
-                        socket.emit('start-generation', {
-                            ...parameters,
-                            model: selected.detail,
-                            clientId: socket.id,
-                        });
-                    });
-            });
-
-            socket.on('generated-text', (text: string) => {
-                vscode.window.showInformationMessage(text);
-                socket.close();
-            });
-
-            socket.on('server-error', (error: any) => {
-                vscode.window.showErrorMessage(error.message || 'An error occurred');
-                socket.close();
-            });
+        socket.on("generated-text", (text: string) => {
+          vscode.window.showInformationMessage(text);
+          socket.close();
         });
-    });
 
-    context.subscriptions.push(disposable);
+        socket.on("server-error", (error: any) => {
+          vscode.window.showErrorMessage(error.message || "An error occurred");
+          socket.close();
+        });
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
