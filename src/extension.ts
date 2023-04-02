f65ca9e7a7718247be8b602f52616d7daa4fa9a6
Wenbin Liu <liuwenbin.lwb@gmail.com>
2023-04-02 19:55:01 +0800

    simple implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,53 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import axios from "axios";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "mychatgpt" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mychatgpt" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "startMyChatGpt",
+    async () => {
+      const userInput = await vscode.window.showInputBox({
+        prompt: "请输入问题",
+        value: "",
+      });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('startMyChatGpt', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MyChatGpt!');
+      let config = vscode.workspace.getConfiguration("startMyChatGpt");
+      const openaiApiKey = config.get("openaiApiKey");
 
-		const panel = vscode.window.createWebviewPanel(
-			'myChatGPT',
-			'My ChatGPT',
-			vscode.ViewColumn.Two,
-			{
-			  enableScripts: true,
-			  retainContextWhenHidden: true
-			}
-		  );
+      try {
+        const response = await axios.post(
+          "https://api.openai.com/v1/engines/davinci-codex/completions",
+          {
+            prompt: userInput,
+            max_tokens: 60,
+            temperature: 0.7,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${openaiApiKey}`,
+            },
+          }
+        );
+        const answer = response.data.choices[0].text;
+        vscode.window.showInformationMessage(answer);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  );
 
-		  panel.webview.html = `
-			<!DOCTYPE html>
-			<html lang="en">
-				<head>
-				<meta charset="UTF-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>MyChatGPT</title>
-				</head>
-				<body>
-				<div id="app"></div>
-				<div>这是个自定义的界面</div>
-				</body>
-			</html>
-			`;
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
